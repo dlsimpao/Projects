7d9da7c0dcc0413d03569c90a77efa427390b4b4
Ricardo Simpao <rlsimpao@ucdavis.edu>
2020-01-30 09:39:39 -0800

    Update Web Scraper.py
    new source

--- Web Scraper.py
+++ Web Scraper.py
@@ -1,30 +1,48 @@
 #Web scraper
-#Source: https://www.edureka.co/blog/web-scraping-with-python/
+#Source: https://realpython.com/python-web-scraping-practical-introduction/
 
-from selenium import webdriver
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
 from bs4 import BeautifulSoup
-import pandas as pd
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
 
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
-driver.get("<a href='https://www.flipkart.com/laptops/'>https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
-
-#Extracting data from the website
-
-content = driver.page_source
-soup = BeautifulSoup(content)
-for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
-    name=a.find('div', attrs={'class':'_3wU53n'})
-    price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-    rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-    products.append(name.text)
-    prices.append(price.text)
-    ratings.append(rating.text)
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
 
 
-#Store
-df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
-df.to_csv('products.csv', index=False, encoding='utf-8')
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+
+raw_html = simple_get("http://www.fabpedigree.com/james/mathmen.htm")
+html = BeautifulSoup(raw_html, 'html.parser')
+for i, li in enumerate(html.select('li')):
+    print(i,li.text)
